19db858903916deb03513d0f1fe1a5510214734f
localuser <localuser@VEA.LV>
2026-02-25 10:30:02 +0200

    Add: added variables and getters in Student class

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,5 +1,33 @@
 package model;
 
+//modelu klase
 public class Student {
-
+	//1. mainigie
+	private long studId;
+	private String name;
+	private String surname;
+	private String personCode;
+	
+	//2. get funkcijas
+	public long getStudId() {
+		return studId;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getSurname() {
+		return surname;
+	}
+	
+	public String getPersonCode() {
+		return personCode;
+	}
+	
+	//3. set funkcijas
+	//4.1. bezargumenta konstruktors
+	//4.2. argumuneta konstruktors
+	//5. toString funkcija
+	//6. visas parejas funkcijas pec nepieciesamibas
 }
