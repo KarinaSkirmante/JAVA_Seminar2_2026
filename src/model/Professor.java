542abea489bd8d032af518f7aafe77b75fc2054f
localuser <localuser@VEA.LV>
2026-02-25 12:02:49 +0200

    Edit: added setters in professor class

--- src/model/Professor.java
+++ src/model/Professor.java
@@ -10,6 +10,8 @@ public class Professor {
 	private String personCode;
 	private ProfDegree degree;
 	
+	private static long counter = 10000;
+	
 	//2. getters
 	public long getProfId() {
 		return profId;
@@ -27,9 +29,59 @@ public class Professor {
 		return degree;
 	}
 	
-
 	
 	//3. setters
+	public void setProfId() {
+		profId = counter;
+		counter++;
+	}
+		
+	public void setName(String inputName) {
+		if((inputName != null) && (!inputName.isEmpty()) && (inputName.matches("[A-Z]{1}[a-z]{2,15}([ ]{1}[A-Z]{1}[a-z]{2,15})?"))    )
+		{
+			name = inputName;
+		}
+		else
+		{
+			name = "Unknown";
+		}
+	}
+	
+	
+	public void setSurname(String inputSurname) {
+		if((inputSurname != null) && (!inputSurname.isEmpty()) 
+				&& (inputSurname.matches("[A-Z]{1}[a-z]{2,15}([-]{1}[A-Z]{1}[a-z]{2,15})?"))    )
+		{
+			surname = inputSurname;
+		}
+		else
+		{
+			surname = "Unknown";
+		}
+	}
+	
+	public void setPersonCode(String inputPersonCode) {
+		//TODO regex masku paskatities, lai ir atbilstosi dienu skaitam konkreta menesi
+		if((inputPersonCode != null) && (!inputPersonCode.isEmpty()) 
+				&& (inputPersonCode.matches("[0-9]{6}[-]{1}[0-9]{5}")) ) {
+			personCode = inputPersonCode;
+		}
+		else
+		{
+			personCode = "Unknown";
+		}
+	}
+	
+	public void setDegree(ProfDegree inputDegree) {
+		if(inputDegree!=null) {
+			degree = inputDegree;
+		}
+		else
+		{
+			degree = ProfDegree.unknown;
+		}
+	}
+
 	
 	
 	//4.1. bez ar gumenta konstruktors
