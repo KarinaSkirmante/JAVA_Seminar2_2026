a9f82d4dc9c7a5ca30131b619652a80208dcefe8
localuser <localuser@VEA.LV>
2026-02-25 11:17:05 +0200

    Edit: added setters in Stduent class

--- src/model/Student.java
+++ src/model/Student.java
@@ -8,6 +8,9 @@ public class Student {
 	private String surname;
 	private String personCode;
 	
+	//palgmainigais, kam nevajag ne set, ne get
+	private static long counter = 0;
+	
 	//2. get funkcijas
 	public long getStudId() {
 		return studId;
@@ -26,6 +29,47 @@ public class Student {
 	}
 	
 	//3. set funkcijas
+	public void setStudId() {
+		studId = counter;
+		counter++;
+	}
+
+	public void setName(String inputName) {
+		if((inputName != null) && (!inputName.isEmpty()) && (inputName.matches("[A-Z]{1}[a-z]{2,15}([ ]{1}[A-Z]{1}[a-z]{2,15})?"))    )
+		{
+			name = inputName;
+		}
+		else
+		{
+			name = "Unknown";
+		}
+	}
+	
+	
+	public void setSurname(String inputSurname) {
+		if((inputSurname != null) && (!inputSurname.isEmpty()) 
+				&& (inputSurname.matches("[A-Z]{1}[a-z]{2,15}([-]{1}[A-Z]{1}[a-z]{2,15})?"))    )
+		{
+			surname = inputSurname;
+		}
+		else
+		{
+			surname = "Unknown";
+		}
+	}
+	
+	public void setPersonCode(String inputPersonCode) {
+		//TODO regex masku paskatities, lai ir atbilstosi dienu skaitam konkreta menesi
+		if((inputPersonCode != null) && (!inputPersonCode.isEmpty()) 
+				&& (inputPersonCode.matches("[0-9]{6}[-]{1}[0-9]{5}")) ) {
+			personCode = inputPersonCode;
+		}
+		else
+		{
+			personCode = "Unknown";
+		}
+	}
+	
 	//4.1. bezargumenta konstruktors
 	//4.2. argumuneta konstruktors
 	//5. toString funkcija
