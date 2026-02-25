8080682e265fc4bf75438fa7984286b0e493acf5
localuser <localuser@VEA.LV>
2026-02-25 11:55:27 +0200

    Add: added enum for professor degree and professor class variables

---
+++ src/model/Professor.java
@@ -0,0 +1,20 @@
+package model;
+
+import model.enums.ProfDegree;
+
+public class Professor {
+	//1. mainigie
+	private long profId;
+	private String name;
+	private String surname;
+	private String personCode;
+	private ProfDegree degree;
+	
+	//2. getters
+	//3. setters
+	//4.1. bez ar gumenta konstruktors
+	//4.2. argumenta konstruktors
+	//5. toString vunkaijas
+	//6. citas funkcijas (ja nepieciesams)
+
+}
