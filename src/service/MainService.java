a6b59e561aa5d1e3fe2d236e60f6b9fd3baf3d95
KarinaSkirmante <karina.krinkele@gmail.com>
2026-03-11 09:21:58 +0200

    Edit: added some professors obkects in MainService

--- src/service/MainService.java
+++ src/service/MainService.java
@@ -1,10 +1,14 @@
 package service;
 
+
+import model.Professor;
 import model.Student;
+import model.enums.ProfDegree;
 
 public class MainService {
 
 	public static void main(String[] args) {
+		System.out.println("----STUDENTS-------------");
 		//datu tips  nosaukums   = new konstruktors;
 		Student      stud1       = new Student();
 		System.out.println(stud1);
@@ -12,6 +16,16 @@ public class MainService {
 		System.out.println(stud2);
 		Student      stud3       = new Student("671547", "Ser2d", null);
 		System.out.println(stud3);
+		
+		System.out.println("----PROFESSORS-------------");
+		
+		Professor prof1 = new Professor();
+		System.out.println(prof1);
+		Professor prof2 = new Professor("Vairis", "Caune", "120914-12121", ProfDegree.phd);
+		System.out.println(prof2);
+		Professor prof3 = new Professor("Galina", "Hilkevica", "121298-34567", ProfDegree.phd);
+		System.out.println(prof3);
+		
 	}
 
 }
