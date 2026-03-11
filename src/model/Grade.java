70ade8bdb66c6752420622e3bd5135854dad6681
KarinaSkirmante <karina.krinkele@gmail.com>
2026-03-11 10:46:54 +0200

    Edit: added both constructors in the Grade class

--- src/model/Grade.java
+++ src/model/Grade.java
@@ -62,11 +62,24 @@ public class Grade {
 		}
 	}
 	
-
-	
-	
 	//4. bezargumenta konstruktors
+	public Grade() {
+		setGradeId();
+		setGrValue(1);
+		setStudent(new Student());
+		setCourse(new Course());
+		
+		
+	}
+	
+
 	//5.argumenta konstruktors
+	public Grade(int inputGrValue, Student inputStudent, Course inputCourse) {
+		setGradeId();
+		setGrValue(inputGrValue);
+		setStudent(inputStudent);
+		setCourse(inputCourse);
+	}
 	//6. toString
 	//7. parejas funkcijas
 	
